cc3e509eb80d1285423b67239f7cb95c4cb7db0f
Gabe Espinosa <gabriel.je@berkeley.edu>
2021-11-11 16:16:03 -0800

    transactions functions

---
+++ types/schema.ts
@@ -0,0 +1,8 @@
+export type Transaction = {
+    admin_name: string;
+    date: Date | string;
+    description: string | null;
+    family_id: string;
+    point_gain: number;
+    user_name: string;
+};
