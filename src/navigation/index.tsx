252f1fb0b75ce540967e02af16e05a780efb3a30
danashimessele <danashimessele@berkeley.edu>
2022-02-27 15:10:18 -0800

    ran npm install, conflcits seem resolved

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -9,7 +9,6 @@ import {
   NavigationContainer,
   DefaultTheme,
   DarkTheme,
-<<<<<<< HEAD
 } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
@@ -21,19 +20,6 @@ import LinkingConfiguration from './LinkingConfiguration';
 import TabNavigator from './TabNavigator';
 import LoginStack from './LoginStack';
 import ProfileScreen from '../screens/Profile/ProfileScreen';
-=======
-} from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import InviteScreen from "../screens/Invite/InviteScreen";
-import ModalScreen from "../screens/ModalScreen";
-import NotFoundScreen from "../screens/NotFoundScreen";
-import { RootStackParamList } from "../types/navigation";
-import LinkingConfiguration from "./LinkingConfiguration";
-import TabNavigator from "./TabNavigator";
-import LoginStack from "./LoginStack";
-import ProfileScreen from "../screens/ProfileScreen";
->>>>>>> 8cab1852a1171bf79e322200319572003383b713
 
 /**
  * A root stack navigator is often used for displaying modals on top of all other content.
