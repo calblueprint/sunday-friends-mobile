245554bce18917d6c9d28fef1bdd2f896b18b087
Yulin <yulinli@Yulins-MacBook-Pro-2.local>
2022-03-01 11:34:11 -0800

    hafl finished initial screen

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -4,32 +4,88 @@ import EditScreenInfo from '../../components/EditScreenInfo';
 import { getAllTransactions } from '../../firebase/firestore/transaction';
 import { useEffect } from 'react';
 import ViewContainer from '../../components/ViewContainer';
-import { Title } from 'react-native-paper';
+import { Divider, Title } from 'react-native-paper';
 import styles from './styles';
 
 const HomeScreen = ({navigation}: any) => {
-
-    // USED FOR MANUAL TESTING, FEEL FREE TO COMMENT OUT/DELETE
-    // useEffect(() => {
-    //     const test = async () => {
-    //         const allTs = await getAllTransactions();
-    //         console.log(allTs);
-    //         if (allTs) {
-    //             console.log(allTs[0])
-    //         }
-    //     }
-    //     test();
-    // }, []);
-
     return (
-        <ViewContainer>
-            <Title>Home Screen</Title>
-            <TouchableOpacity style={styles.profile} onPress={() => navigation.navigate('Profile')}>
-                <Text>Profile</Text>
-            </TouchableOpacity>
-            <EditScreenInfo path="/screens/HomeScreen.tsx" />
-        </ViewContainer>
+        <View>
+            <View style = {styles.headline}>
+                <Text style={styles.title1}>Kim Family</Text>
+                <View style={styles.innerContainer}>
+                    <View style={styles.addCircle} />
+                </View>
+            </View>
+
+            <View style={styles.familyCardContainer}>
+                <View style={styles.addContainer}>
+                    <View style={styles.addCircle} />
+                    <View style={styles.addCircle} />
+                    <View style={styles.addCircle} />
+                    <View style={styles.addCircle} />
+                <View style={styles.innerContainer}>
+                    <View style={styles.rowContainer}>
+                        <Text style={styles.nameText}>5 People</Text>
+                    </View>
+                        <Text>Me, Jacob, Cindy, Albert, Charles</Text>
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.familyBalanceCardContainer}>
+                <View style={styles.addContainer}>
+                    <View style = {styles.columnContainer}>
+                        <Text style={styles.balanceText}>9088</Text>
+                        <Text style={styles.nameText}>Account Balance</Text>
+                    </View>
+                </View>
+
+            </View>
+
+            
+        
+            
+        </View>
+        
     );
 }
 
 export default HomeScreen;
+
+// import * as React from "react";
+// import { useNavigation } from "@react-navigation/native";
+// import { View, Text, Pressable } from "react-native";
+// import RectangularButton from "../../components/RectangularButton";
+// import { default as styles } from "./styles";
+// import globalStyles from "../../globalStyles";
+// import MemberCard from "./MemberCard";
+
+// const InviteScreen = () => {
+//   return (
+//     <View style={globalStyles.container}>
+//       <Text style={styles.title1}>You're in charge!</Text>
+//       <Text style={styles.title2}>Invite members to Kim Family</Text>
+//       <MemberCard
+//         name="Yakob Kim"
+//         head={true}
+//         email="yakobkimiscool@gmail.com"
+//         status="hi"
+//       />
+    //   <Pressable style={styles.backgroundContainer}>
+    //     <View style={styles.addContainer}>
+    //       <View style={styles.addCircle} />
+    //       <View style={styles.innerContainer}>
+    //         <View style={styles.rowContainer}>
+    //           <Text style={styles.nameText}>Add a member</Text>
+    //         </View>
+    //         <Text>Invite parents and/or children</Text>
+    //       </View>
+    //     </View>
+    //   </Pressable>
+
+//       <RectangularButton text="review" />
+//     </View>
+//   );
+// };
+
+// export default InviteScreen;
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,8 +1,88 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    profile: {
-        position: "absolute",
-        top: 20,
-    }
-})
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+    backgroundColor: "#eee",
+  },
+
+  title1: {
+    fontFamily: "DM Sans",
+    fontSize: 28,
+    fontWeight: "500",
+    textAlign: "left",
+    width: "80%",
+  },
+
+  title2: {
+    fontFamily: "DM Sans",
+    fontSize: 30,
+    fontWeight: "700",
+    textAlign: "left",
+    width: "80%",
+    marginVertical: "2%",
+  },
+
+  familyCardContainer: {
+    backgroundColor: "#F6F6F6",
+    alignItems: "stretch",
+    marginTop: "5%",
+    borderRadius: 10, 
+  },
+
+  familyBalanceCardContainer: {
+    backgroundColor: "#F6F6F6",
+    alignItems: "stretch",
+    marginTop: "5%",
+    borderRadius: 10, 
+  },
+
+  headline: {
+    flexDirection: "row",
+    alignItems: "stretch",
+    marginTop: "20%",
+  },
+
+  addContainer: {
+    flexDirection: "row",
+    marginVertical: "6%",
+    width: "80%",
+  },
+
+  addCircle: {
+    backgroundColor: "#C4C4C4",
+    width: 30,
+    height: 30,
+    borderRadius: 25,
+    justifyContent: "center",
+  },
+
+  innerContainer: {
+    alignSelf: "center",
+    marginLeft: "3%",
+  },
+
+  rowContainer: {
+    flexDirection: "row",
+  },
+
+  columnContainer: {
+    marginLeft: "5%",
+    flexDirection: "column",
+  },
+
+  nameText: {
+    fontFamily: "Avenir",
+    fontWeight: "800",
+    fontSize: 14,
+  },
+
+  balanceText: {
+    fontFamily: "Avenir",
+    fontWeight: "900",
+    fontSize: 28,
+
+  },
+});
