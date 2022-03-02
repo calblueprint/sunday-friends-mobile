b784c4b276bdec93eefffad8a6f64f65874d5122
Jacob Kim <jacobjk01@berkeley.edu>
2022-03-02 01:40:55 -0800

    backend migration, using yarn instead

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,3 +1,5 @@
+import firebase from 'firebase/app';
+
 export type Transaction = {
     admin_name: string;
     date: Date | string;
@@ -6,3 +8,27 @@ export type Transaction = {
     point_gain: number;
     user_name: string;
 };
+
+export type Family = {
+    total_points: number;
+    family_name: string;
+    user_ids: User[];
+    family_id: string;
+  };
+
+  export type User = {
+    user_id: string;
+    address: string;
+    created_at: Date | firebase.firestore.Timestamp | string;
+    email: string;
+    role: string;
+    family_id: number;
+    full_name: string;
+    last_active: Date | firebase.firestore.Timestamp;
+    parent: boolean;
+    points: number;
+    reward_eligible: boolean;
+    suspended: boolean;
+    phone_number: string;
+    transactions: Transaction[];
+  };
