057e2cb18ef3a5e4b7479e314a9d793124a03ac7
danashimessele <danashimessele@berkeley.edu>
2022-03-10 01:14:49 -0800

    modal almost ready to design, need to fix background opacity

--- assets/SvgIcon.tsx
+++ assets/SvgIcon.tsx
@@ -1,26 +1,53 @@
 import * as React from 'react';
 import { SvgXml } from 'react-native-svg';
 
-export type IconType =
-  | "chevron_down"
+export type IconType = 'chevron_down' | 'greyX' | 'addPlus' | 'downArrow';
 
 const IconSvgs: Record<IconType, React.ReactElement> = {
-    chevron_down: (
-      <SvgXml xml={`
+  chevron_down: (
+    <SvgXml
+      xml={`
       <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M10 13.0937L4.9917 8.08542L6.1692 6.90625L10 10.7396L13.83 6.90625L15.0084 8.08458L10 13.0937Z" fill="#A9A9A9"/>
       </svg>
-      `}>
-      </SvgXml>
-    )
-}
+      `}
+    />
+  ),
+  greyX: (
+    <SvgXml
+      xml={`
+      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="8.5" cy="8.5" r="8.5" fill="#CCCCCC"/>
+      <path d="M7.41759 8.45583L5.09709 5.104H7.04075L8.53817 7.56333L9.97609 5.104H11.8503L9.57942 8.42608L12.1181 12.125H10.1744L8.45884 9.29875L6.79284 12.125H4.95825L7.41759 8.45583Z" fill="#9D9D9D"/>
+      </svg>
+      `}
+    />
+  ),
+  addPlus: (
+    <SvgXml
+      xml={`
+      <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path opacity="0.8" d="M3.40907 2.90145V0.368888H4.58326V2.90145H7.11582V4.07563H4.58326V6.60819H3.40907V4.07563H0.876517V2.90145H3.40907Z" fill="#525454"/>
+      </svg>
+      `}
+    />
+  ),
+  downArrow: (
+    <SvgXml
+      xml={`
+      <svg width="19" height="8" viewBox="0 0 19 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 1L9.5 6L18 1" stroke="#C4C4C4" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+     `}
+    />
+  )
+};
 
 type Props = {
-    type: IconType;
-  };
+  type: IconType;
+};
 
-const SvgIcon: React.FC<Props> = ({ type }: Props) => {
-    return React.cloneElement(IconSvgs[type]);
-}
+const SvgIcon: React.FC<Props> = ({ type }: Props) =>
+  React.cloneElement(IconSvgs[type]);
 
-export default SvgIcon
+export default SvgIcon;
--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,61 +1,172 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { Modal, StyleSheet, View, Image, ImageBackground, Text, Pressable } from 'react-native';
-import { Title } from 'react-native-paper';
-import EditScreenInfo from '../../components/EditScreenInfo';
-import ViewContainer from '../../components/ViewContainer';
-import styles from '../Profile/styles';
+import * as React from "react";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  ImageBackground,
+  Text,
+  Pressable,
+  Modal,
+  FlatList,
+} from "react-native";
+import { Title } from "react-native-paper";
+import EditScreenInfo from "../../components/EditScreenInfo";
+import ViewContainer from "../../components/ViewContainer";
+import styles from "./styles";
+import SvgIcon from "../../../assets/SvgIcon";
+import globalStyles from "../../globalStyles";
 
-
-const ProfileScreen = () => {
-
-    const [profileSwitchModalVisible, setProfileSwitchModalVisible] = useState(false);
-
+function Item({ name, role }) {
+  if (role == 'Head') {
     return (
-        <ViewContainer>
-            <Modal
-                visible={profileSwitchModalVisible}
-                onRequestClose={() => setProfileSwitchModalVisible(false)}
-                transparent={true}
-            >
-                <View
-                    style={styles.switchProfileModalView}
-                    
-                >
-                    <Text> hello</Text>
-                </View>
-            </Modal>
-            <Pressable
-                style={styles.profileImagePressable}
-                onPress={() => setProfileSwitchModalVisible(!profileSwitchModalVisible)}
-            >
-                <Image style = {styles.profileImage} source={require('../../../assets/images/Smileys.png')} />
-            </Pressable>
-            <Title style = {styles.profileName}>Jacob Kim</Title>
-            <Pressable style={styles.editPressable}>
-                <Text style={styles.buttonText}>EDIT PROFILE</Text>
-            </Pressable>
-            <Pressable style={styles.resetPressable}>
-                <Text style={styles.buttonText}>RESET PASSWORD</Text>
-            </Pressable>
-            <View style={styles.profileFields}>
-                <Text style={styles.fields}>NAME</Text>
-                <Text style={styles.fields}>ROLE</Text>
-                <Text style={styles.fields}>EMAIL</Text>
-                <Text style={styles.fields}>PASSWORD</Text>
-            </View>
-            <View style={styles.profileValues}>
-                <Text style={styles.values}>Jacob Kim</Text>
-                <Text style={styles.values}>Dad</Text>
-                <Text style={styles.values}>booblywobbly@gmail.com</Text>
-                <Text style={styles.values}>********</Text>
-            </View>
-            <Pressable style={styles.logoutPressable}>
-                <Text style={styles.buttonText}>LOGOUT</Text>
-            </Pressable>
-        </ViewContainer>
+      <View style={styles.headFlatListItem}>
+        <View style={styles.photoFiller} />
+        <View style={styles.listInfo}>
+          <Text style={globalStyles.body1Bold}>{name}</Text>
+          <Text style={globalStyles.body2}>{role}</Text>
+        </View>
+        <Pressable style={styles.signedIn}>
+          <Text style={globalStyles.body2}>Signed In</Text>
+        </Pressable>
+      </View>
     );
+  }
+  return (
+    <View style={styles.flatListItem}>
+      <View style={styles.photoFiller} />
+      <View style={styles.listInfo}>
+        <Text style={globalStyles.body1Bold}>{name}</Text>
+        <Text style={globalStyles.body2}>{role}</Text>
+      </View>
+      <Pressable style={styles.switch}>
+        <Text style={globalStyles.body2}>Switch</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  const [profileSwitchModalVisible, setProfileSwitchModalVisible] =    useState(false);
+
+  const DATA = [
+    {
+      id: "1",
+      name: "Kelly Hu",
+      role: "Dependent",
+    },
+    {
+      id: "2",
+      name: "Albert Liu",
+      role: "Dependent",
+    },
+    {
+      id: "3",
+      name: "Sydney Bui",
+      role: "Dependent",
+    },
+    // {
+    //     id: "4",
+    //     name: "Gabe Espinosa",
+    //     role: "Dependent",
+    //   },
+    //   {
+    //     id: "5",
+    //     name: "Danashi Messele",
+    //     role: "Dependent",
+    //   },
+  ];
+
+  const renderItem = ({ item }) => <Item name={item.name} role={item.role} />;
+
+  const itemSeparator = () => <View style={styles.itemSeparator} />;
+
+  const addMember = () => (
+    <View style={styles.addMember}>
+      <Pressable style={styles.addPressable}>
+        <SvgIcon type="addPlus" />
+      </Pressable>
+      <Text style={[globalStyles.body1Bold, styles.listInfo]}>Add Member</Text>
+    </View>
+  );
+
+  if (profileSwitchModalVisible) {
+    return (
+      <View style={styles.modalVisibleContainer}>
+        <Pressable style={styles.profileImagePressable}>
+          <Image
+            style={styles.profileImage}
+            source={require("../../../assets/images/CrownedSmiley.png")}
+          />
+          <View style={styles.downArrow}>
+            <SvgIcon type="downArrow" />
+          </View>
+        </Pressable>
+        <Modal visible={profileSwitchModalVisible} transparent>
+          <View style={styles.switchProfileModalView}>
+            <Pressable
+              style={styles.switchModalX}
+              onPress={() => setProfileSwitchModalVisible(false)}
+            >
+              <SvgIcon type="greyX" />
+            </Pressable>
+            <Item name="Jacob Kim" role="Head" />
+            <View style={styles.modalDivider} />
+            <FlatList
+              style={styles.flatList}
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+              ItemSeparatorComponent={itemSeparator}
+              ListFooterComponent={addMember}
+            />
+          </View>
+        </Modal>
+        <Pressable style={styles.logoutPressable}>
+          <Text style={globalStyles.overline2}>LOGOUT</Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+  return (
+    <ViewContainer>
+      <Pressable 
+        style={styles.profileImagePressable}
+        onPress={() => setProfileSwitchModalVisible(true)}>
+        <Image
+          style={styles.profileImage}
+          source={require('../../../assets/images/CrownedSmiley.png')}
+        />
+        <View style={styles.downArrow}>
+          <SvgIcon type="downArrow" />
+        </View>
+      </Pressable>
+      <Title style={[styles.profileName, globalStyles.h3Bold]}>Jacob Kim</Title>
+      <Pressable style={styles.editPressable}>
+        <Text style={globalStyles.overline2}>EDIT PROFILE</Text>
+      </Pressable>
+      <Pressable style={styles.resetPressable}>
+        <Text style={globalStyles.overline2}>RESET PASSWORD</Text>
+      </Pressable>
+      <View style={styles.profileFields}>
+        <Text style={globalStyles.overline1}>NAME</Text>
+        <Text style={globalStyles.overline1}>ROLE</Text>
+        <Text style={globalStyles.overline1}>EMAIL</Text>
+        <Text style={globalStyles.overline1}>PASSWORD</Text>
+      </View>
+      <View style={styles.profileValues}>
+        <Text style={globalStyles.body1}>Jacob Kim</Text>
+        <Text style={globalStyles.body1}>Dad</Text>
+        <Text style={globalStyles.body1}>booblywobbly@gmail.com</Text>
+        <Text style={globalStyles.body1}>********</Text>
+      </View>
+      <Pressable style={styles.logoutPressable}>
+        <Text style={globalStyles.overline2}>LOGOUT</Text>
+      </Pressable>
+    </ViewContainer>
+  );
 }
 
 export default ProfileScreen;
