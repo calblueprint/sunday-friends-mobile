316868d02cde28c55fa869db7001e8042fe8d5a0
danashimessele <90217967+danashimessele@users.noreply.github.com>
2022-03-13 23:27:50 -0700

    Danashi - profile switch modal (#19)
    * before ediiting anything. package lock?
    
    * added profile picture, name, and some of the fields; need to fix css
    
    * setup page structure
    
    * fixed some css to clean it up
    
    * ran npm install, conflcits seem resolved
    
    * cleaned up css
    
    * intial modal work
    
    * switching branches
    
    * modal started, installing yarn and changing versions
    
    * yarnlock
    
    * deleted packagelock
    
    * changed node to 14.17.0 using nvmWindows, yarn installed
    
    * modal almost ready to design, need to fix background opacity
    
    * some reformatting and cleaned up code a bit, also pulled origin main
    
    * made modal into component
    
    * removed the three eslint packages from messenger
    
    * raised css to hifi, modal is mostly complete
    
    * changed everything to const, made flatlistitem component, styling changes were made in backend branch

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,40 +1,83 @@
-import * as React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { Button, StyleSheet, View, Image, ImageBackground, Text, Pressable } from 'react-native';
-import { Title } from 'react-native-paper';
-import EditScreenInfo from '../../components/EditScreenInfo';
-import ViewContainer from '../../components/ViewContainer';
-import styles from '../Profile/styles';
+import * as React from "react";
+import { useState, useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  ImageBackground,
+  Text,
+  Pressable,
+  Modal,
+  FlatList,
+} from "react-native";
+import { Title } from "react-native-paper";
+import EditScreenInfo from "../../components/EditScreenInfo";
+import ViewContainer from "../../components/ViewContainer";
+import styles from "./styles";
+import SvgIcon from "../../../assets/SvgIcon";
+import globalStyles from "../../globalStyles";
+import ProfileSwitchModal from "../../components/ProfileSwitchModal/ProfileSwitchModal";
+import userContext from "../../context/userContext";
+import { getUser } from "../../firebase/firestore/user";
+import { User } from "../../types/schema";
+import ProfileSwitchItem from "../../components/ProfileSwitchItem/ProfileSwitchItem";
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ navigation }: any) => {
+  const [profileSwitchModalVisible, setProfileSwitchModalVisible] =
+    useState(false);
 
-    return (
-        <ViewContainer>
-            <Image style = {styles.profileImage} source={require('../../../assets/images/Smileys.png')} />
-            <Title style = {styles.profileName}>Jacob Kim</Title>
-            <Pressable style={styles.editPressable}>
-                <Text style={styles.buttonText}>EDIT PROFILE</Text>
-            </Pressable>
-            <Pressable style={styles.resetPressable}>
-                <Text style={styles.buttonText}>RESET PASSWORD</Text>
-            </Pressable>
-            <View style={styles.profileFields}>
-                <Text style={styles.fields}>NAME</Text>
-                <Text style={styles.fields}>ROLE</Text>
-                <Text style={styles.fields}>EMAIL</Text>
-                <Text style={styles.fields}>PASSWORD</Text>
-            </View>
-            <View style={styles.profileValues}>
-                <Text style={styles.values}>Jacob Kim</Text>
-                <Text style={styles.values}>Dad</Text>
-                <Text style={styles.values}>booblywobbly@gmail.com</Text>
-                <Text style={styles.values}>********</Text>
-            </View>
-            <Pressable style={styles.logoutPressable}>
-                <Text style={styles.buttonText}>LOGOUT</Text>
-            </Pressable>
-        </ViewContainer>
-    );
-}
+  return (
+    <ViewContainer>
+      <View
+        style={profileSwitchModalVisible ? styles.modalVisibleContainer : null}
+      />
+
+      <ProfileSwitchModal
+        visible={profileSwitchModalVisible}
+        setVisible={setProfileSwitchModalVisible}
+      />
+      <Pressable style={styles.backArrowPressable}>
+        <SvgIcon type="chevronLeft" />
+      </Pressable>
+      <Pressable
+        style={styles.profileImagePressable}
+        onPress={() => setProfileSwitchModalVisible(true)}
+      >
+        <Image
+          style={styles.profileImage}
+          source={require("../../../assets/images/CrownedSmiley.png")}
+        />
+        <View style={styles.downArrow}>
+          <SvgIcon type="downArrow" />
+        </View>
+      </Pressable>
+
+      <Title style={[styles.profileName, globalStyles.h3Bold]}>Jacob Kim</Title>
+      <Pressable style={styles.editPressable}>
+        <Text style={styles.overline2WHITE}>EDIT PROFILE</Text>
+      </Pressable>
+      <Pressable style={styles.resetPressable}>
+        <Text style={globalStyles.overline2}>RESET PASSWORD</Text>
+      </Pressable>
+
+      <View style={styles.profileFields}>
+        <Text style={globalStyles.overline1}>NAME</Text>
+        <Text style={globalStyles.overline1}>ROLE</Text>
+        <Text style={globalStyles.overline1}>EMAIL</Text>
+      </View>
+
+      <View style={styles.profileValues}>
+        <Text style={globalStyles.body1}>Jacob Kim</Text>
+        <Text style={globalStyles.body1}>Dad</Text>
+        <Text style={globalStyles.body1}>booblywobbly@gmail.com</Text>
+      </View>
+
+      <Pressable style={styles.logoutPressable}>
+        <Text style={styles.overline1WHITE}>LOGOUT</Text>
+      </Pressable>
+    </ViewContainer>
+  );
+};
 
 export default ProfileScreen;
