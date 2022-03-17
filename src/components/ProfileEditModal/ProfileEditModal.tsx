651398aa3387c73690f5e1d89e6825867f23d262
danashimessele <danashimessele@berkeley.edu>
2022-03-17 02:14:49 -0700

    saving values to backend, but small bug where in-modal text  doesnt update until second press to edit, but updates right away on the main profile screen.

--- src/components/ProfileEditModal/ProfileEditModal.tsx
+++ src/components/ProfileEditModal/ProfileEditModal.tsx
@@ -1,12 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import styles from "./styles";
 import { Modal, View, Pressable, TextInput, Text } from "react-native";
 import SvgIcon from "../../../assets/SvgIcon";
 import globalStyles from "../../globalStyles";
+import { User } from "../../types/schema";
+import userContext from "../../context/userContext";
+import firebaseApp from "../../firebase/firebaseApp";
+import { getUser } from "../../firebase/firestore/user";
 
-export const ProfileEditModal = ({ visible, setVisible, user }: any) => {
+export type UserUpdateData = {
+  full_name?: string;
+  role?: string;
+};
+
+type ProfileEditModalProps = {
+  visible: boolean;
+  setVisible: Function;
+  user: User;
+  setUser: any;
+};
+
+export const ProfileEditModal = ({
+  visible,
+  setVisible,
+  user,
+  setUser,
+}: ProfileEditModalProps) => {
+  const db = firebaseApp.firestore().collection("users");
+  const value = useContext(userContext);
+
   const [nameText, onChangeNameText] = useState(user.full_name);
   const [roleText, onChangeRoleText] = useState(user.role);
+
+  const [newUserData, setNewUserData] = useState<UserUpdateData>({});
+  const [newUserName, setNewUserName] = useState("");
+  const [newUserRole, setNewUserRole] = useState("");
+  const [wasEdited, setWasEdited] = useState(false);
+
+  const refresh = () => {};
+
+  useEffect(() => {
+    getUser(value).then((currUser) => {
+      setUser(currUser);
+      onChangeNameText(newUserName);
+      onChangeRoleText(newUserRole);
+    });
+  }, [nameText]);
+
+  const resetFields = (): void => {
+    getUser(value).then((user) => {
+      setUser(user);
+      onChangeNameText(user.full_name);
+      onChangeRoleText(user.role);
+    });
+  };
+
+  const handleSubmit = async (): Promise<void> => {
+    try {
+      db.doc(user.user_id).update({
+        full_name: newUserName,
+        role: newUserRole,
+      });
+      setVisible(false);
+      console.log("completed");
+    } catch (err) {
+      console.log(0);
+    }
+  };
 
   return (
     <Modal visible={visible} transparent animationType="slide">
@@ -14,11 +74,7 @@ export const ProfileEditModal = ({ visible, setVisible, user }: any) => {
         <View style={styles.topGreyLine} />
         <Pressable
           style={styles.exitPressable}
-          onPress={() => [
-            setVisible(false),
-            onChangeNameText(user.full_name),
-            onChangeRoleText(user.role),
-          ]}
+          onPress={() => [setVisible(false)]}
         >
           <SvgIcon type="greyX" />
         </Pressable>
@@ -34,14 +90,23 @@ export const ProfileEditModal = ({ visible, setVisible, user }: any) => {
         </View>
         <TextInput
           style={[globalStyles.body1, styles.textNameInput]}
-          onChangeText={onChangeNameText}
+          onChangeText={(e) => {
+            onChangeNameText(e);
+            setNewUserName(e);
+          }}
           value={nameText}
         />
         <TextInput
           style={[globalStyles.body1, styles.textRoleInput]}
-          onChangeText={onChangeRoleText}
+          onChangeText={(e) => {
+            onChangeRoleText(e);
+            setNewUserRole(e);
+          }}
           value={roleText}
         />
+        <Pressable onPress={handleSubmit} style={styles.profileImagePressable}>
+          <Text>SAVEE</Text>
+        </Pressable>
       </View>
     </Modal>
   );
