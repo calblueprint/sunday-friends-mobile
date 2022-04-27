99e76b5a3e43596f44d365c6a6b4601c730cac02
danashimessele <90217967+danashimessele@users.noreply.github.com>
2022-04-27 15:48:43 -0700

    Danashi - profileResetPassword (#29)
    * made intiail screens for reset pw and verify email within the ProfileResetModal component
    
    * added last two screens for set new password and success, need to add more test cases with regex for email, length of code, password requirements, etc, as well as saving the password change to the backend, but base  structure is there as of right now.
    
    * added some check states for length of code, password length and matching, and changing opacity for buttons before being clickable, and icons for passwords
    
    * updated styling to match figma changes, email regex validation implemented
    
    * in progress of password verification check
    
    * finished pw validation, need to add error states next
    
    * forgot to save
    
    * sends email to the given typed email, as well as confirmation email. still in process of fixing small bug with the pin being one off from email and var
    
    * moved emailjs user and service id to env file
    
    * finished up to curr hifi, error states working, sending email and pin verification works, still not yet saving new password to backend.
    
    * cleaned up code a bit and got rid of some unnecessary lines
    
    * added save button for edit, changed role to email, added email error for reset
    
    * added logout functionality, resetting the currentusers password, small styling changes

--- src/components/ProfileLogoutModal/ProfileLogoutModal.tsx
+++ src/components/ProfileLogoutModal/ProfileLogoutModal.tsx
@@ -1,14 +1,44 @@
+import { CommonActions, StackActions } from "@react-navigation/native";
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal, View, Pressable, Text } from "react-native";
+import { signOut } from "../../firebase/auth";
+import firebaseApp from "../../firebase/firebaseApp";
 import globalStyles from "../../globalStyles";
 import styles from "./styles";
 
-export const ProfileLogoutModal = ({ visible, setVisible }: any) => {
+export const ProfileLogoutModal = ({
+  visible,
+  setVisible,
+  logout,
+  setLogout,
+  navigation,
+}: any) => {
+  const handleLogout = () => {
+    setVisible(false);
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "LoginStack" }],
+    });
+  };
+  // useEffect(() => {
+  //   setVisible(false);
+  //   handleLogout();
+  // }, [isLogoutPressed]);
+
   return (
     <Modal visible={visible} transparent>
       <View style={styles.logoutModal}>
-        <Pressable style={styles.logoutPressableModal}>
+        <Pressable
+          style={styles.logoutPressableModal}
+          onPress={() => [
+            navigation.reset({
+              index: 0,
+              routes: [{ name: "LoginStack" }],
+            }),
+            signOut(),
+          ]}
+        >
           <Text style={styles.overline1WHITE}>LOGOUT</Text>
         </Pressable>
         <Pressable
---
+++ src/components/ProfileResetPassword/ProfileResetPassword.tsx
@@ -0,0 +1,388 @@
+import React, { useState } from "react";
+import styles from "./styles";
+import { Modal, View, Pressable, TextInput, Text, Linking } from "react-native";
+import SvgIcon from "../../../assets/SvgIcon";
+import globalStyles from "../../globalStyles";
+import emailjs, { init } from "@emailjs/browser";
+import { EMAILJS_USER_ID, EMAILJS_SERVICE_ID } from "@env";
+import { setUserPassword } from "../../firebase/firestore/user";
+
+export const ProfileResetPassword = ({ visible, setVisible, user }: any) => {
+  const [email, onChangeEmail] = useState("");
+  const [code, onChangeCode] = useState("");
+  const [newPW, onChangeNewPW] = useState("");
+  const [confirmPW, onChangeConfirmPW] = useState("");
+  const [edited, setEdited] = useState(false);
+  const [screen, setScreen] = useState("reset");
+  const [pin, setPin] = useState(0);
+  const [pwError, setPwError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const reset = () => {
+    onChangeEmail("");
+    onChangeCode("");
+    onChangeNewPW("");
+    onChangeConfirmPW("");
+    setEdited(false);
+    setScreen("reset");
+    setVisible(false);
+    setPwError(false);
+    setEmailError(false);
+  };
+
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  init(EMAILJS_USER_ID); //initializes emailJS userID (only 200 emails a month...)
+
+  const resetEmailParams = {
+    from: "Sunday Friends",
+    to: email,
+    name: user.full_name,
+    pin: pin,
+  };
+
+  const successEmailParams = {
+    from: "Sunday Friends",
+    to: email,
+    name: user.full_name,
+  };
+
+  const sendEmail = (type: string) => {
+    var generated = Math.floor(100000 + Math.random() * 900000);
+    setPin(generated);
+    resetEmailParams.pin = generated;
+    switch (type) {
+      case "reset":
+        return emailjs.send(
+          EMAILJS_SERVICE_ID,
+          "template_mz61cu7",
+          resetEmailParams
+        );
+      case "success":
+        return emailjs.send(
+          EMAILJS_SERVICE_ID,
+          "template_dessp0w",
+          successEmailParams
+        );
+    }
+  };
+
+  const valid = () => {
+    switch (screen) {
+      case "reset":
+        return emailRegex.test(email);
+      case "verify":
+        return parseInt(code) == pin;
+      case "setNew":
+        return (
+          newPW == confirmPW &&
+          newPW.length >= 8 &&
+          newPW.length <= 20 &&
+          /\d/.test(newPW) &&
+          /[A-Z]/.test(newPW) &&
+          /^\S+$/.test(newPW)
+        );
+    }
+  };
+
+  const resetModal = () => {
+    return (
+      <Modal visible={visible} transparent animationType="slide">
+        <View style={styles.modalView}>
+          <View style={styles.topGreyLine} />
+          <Pressable style={styles.exitPressable} onPress={() => reset()}>
+            <Text style={[globalStyles.body1, styles.closeText]}>Close</Text>
+          </Pressable>
+          <Text style={[globalStyles.h2, styles.modalTitle]}>
+            Reset Password
+          </Text>
+          <Text style={[globalStyles.h4, styles.subText1]}>
+            Enter the email connected to your account to reset your password.
+          </Text>
+          <Text style={[globalStyles.body1, styles.subText2]}>
+            A text containing a 6-digit code will be sent
+          </Text>
+          <TextInput
+            style={
+              emailError
+                ? [
+                    globalStyles.body1,
+                    styles.emailInputTyping,
+                    styles.errorBackground,
+                  ]
+                : edited && email.length > 0
+                ? [globalStyles.body1, styles.emailInputTyping]
+                : [globalStyles.body1, styles.emailInput]
+            }
+            placeholder="email@gmail.com"
+            placeholderTextColor={"#A9A9A9"}
+            onChangeText={(e) => [onChangeEmail(e), setEdited(true)]}
+            value={email}
+            autoFocus={true}
+          />
+          {emailError ? (
+            <Text style={[globalStyles.body2, styles.emailError]}>
+              Email must match a registered account
+            </Text>
+          ) : null}
+          <Pressable
+            style={
+              valid()
+                ? styles.resetContinuePressableAllowed
+                : styles.resetContinuePressable
+            }
+            onPress={() => {
+              valid() && email == user.email
+                ? [setScreen("verify"), setEdited(false), sendEmail("reset")]
+                : edited
+                ? setEmailError(true)
+                : null;
+            }}
+          >
+            <Text
+              style={
+                valid()
+                  ? [globalStyles.overline1, styles.sapphireText]
+                  : [globalStyles.overline1, styles.greyText]
+              }
+            >
+              CONTINUE
+            </Text>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  };
+
+  const verifyModal = () => {
+    return (
+      <Modal visible={visible} transparent animationType="slide">
+        <View style={styles.modalView}>
+          <View style={styles.topGreyLine} />
+          <Pressable style={styles.exitPressable} onPress={() => reset()}>
+            <Text style={(globalStyles.body1, styles.closeText)}>Close</Text>
+          </Pressable>
+          <Text style={[globalStyles.h2, styles.modalTitle]}>Verify Email</Text>
+          <Text style={[globalStyles.h4, styles.subText1]}>
+            Enter the 6-digit code sent to {email} {pin}
+          </Text>
+          <TextInput
+            style={
+              edited && code.length > 0
+                ? [globalStyles.body1, styles.verifyInputTyping]
+                : [globalStyles.body1, styles.verifyInput]
+            }
+            placeholder="6-digit code"
+            placeholderTextColor={"#A9A9A9"}
+            keyboardType="numeric"
+            onChangeText={(e) => [onChangeCode(e), setEdited(true)]}
+            value={code}
+          />
+          <Pressable
+            style={styles.resendPressable}
+            onPress={() => [
+              setEdited(false),
+              setScreen("verify"),
+              sendEmail("reset"),
+              onChangeCode(""),
+            ]}
+          >
+            <Text style={globalStyles.overline1}>RESEND CODE</Text>
+          </Pressable>
+          <Pressable
+            style={
+              valid()
+                ? styles.verifyContinuePressableAllowed
+                : styles.verifyContinuePressable
+            }
+            onPress={() => {
+              valid() ? [setScreen("setNew"), setEdited(false)] : null;
+            }}
+          >
+            <Text style={[globalStyles.overline1, styles.whiteText]}>
+              CONTINUE
+            </Text>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  };
+
+  const setNewModal = () => {
+    return (
+      <Modal visible={visible} transparent animationType="slide">
+        <View style={styles.modalView}>
+          <View style={styles.topGreyLine} />
+          <Pressable style={styles.exitPressable} onPress={() => reset()}>
+            <Text style={(globalStyles.body1, styles.closeText)}>Close</Text>
+          </Pressable>
+          <Text style={[globalStyles.h2, styles.modalTitle]}>
+            Set New Password
+          </Text>
+          <Text style={[globalStyles.overline2, styles.labelPassword]}>
+            PASSWORD
+          </Text>
+          <TextInput
+            style={
+              pwError
+                ? [
+                    globalStyles.body1,
+                    styles.setNewInputTyping,
+                    styles.errorBackground,
+                  ]
+                : edited && newPW.length > 0
+                ? [globalStyles.body1, styles.setNewInputTyping]
+                : [globalStyles.body1, styles.setNewInput]
+            }
+            placeholder={pwError ? "Error Empty" : "Enter password"}
+            placeholderTextColor="#A9A9A9"
+            onChangeText={(e) => [onChangeNewPW(e), setEdited(true)]}
+            value={newPW}
+            autoFocus={true}
+          />
+          {pwError && newPW.length == 0 ? (
+            <Text style={[globalStyles.body2, styles.fieldReqSetPW]}>
+              Field Required
+            </Text>
+          ) : null}
+          <Text style={[globalStyles.overline2, styles.labelConfirmPassword]}>
+            CONFIRM PASSWORD
+          </Text>
+          <TextInput
+            style={
+              pwError
+                ? [
+                    globalStyles.body1,
+                    styles.confirmNewInputTyping,
+                    styles.errorBackground,
+                  ]
+                : edited && confirmPW.length > 0
+                ? [globalStyles.body1, styles.confirmNewInputTyping]
+                : [globalStyles.body1, styles.confirmNewInput]
+            }
+            placeholder={pwError ? "Error Empty" : "Enter same password"}
+            placeholderTextColor="#A9A9A9"
+            onChangeText={(e) => [onChangeConfirmPW(e), setEdited(true)]}
+            value={confirmPW}
+          />
+
+          {pwError ? (
+            newPW != confirmPW ? (
+              <Text style={[globalStyles.body2, styles.pwMustMatchError]}>
+                Passwords must match
+              </Text>
+            ) : confirmPW.length == 0 ? (
+              <Text style={[globalStyles.body2, styles.fieldReqConfirmPW]}>
+                Field Required
+              </Text>
+            ) : null
+          ) : null}
+
+          <View style={styles.characterCheckIcon}>
+            {newPW.length >= 8 && newPW.length <= 20 ? (
+              <SvgIcon type="greenCheck" />
+            ) : (
+              <SvgIcon type="redX" />
+            )}
+          </View>
+          <View style={styles.characterCheckText}>
+            <Text style={globalStyles.body2}>8-20 Characters</Text>
+          </View>
+          <View style={styles.numberCheckIcon}>
+            {/\d/.test(newPW) ? (
+              <SvgIcon type="greenCheck" />
+            ) : (
+              <SvgIcon type="redX" />
+            )}
+          </View>
+          <View style={styles.numberCheckText}>
+            <Text style={globalStyles.body2}>At least 1 number</Text>
+          </View>
+          <View style={styles.uppercaseCheckIcon}>
+            {/[A-Z]/.test(newPW) ? (
+              <SvgIcon type="greenCheck" />
+            ) : (
+              <SvgIcon type="redX" />
+            )}
+          </View>
+          <View style={styles.uppercaseCheckText}>
+            <Text style={globalStyles.body2}>At least 1 uppercase letter</Text>
+          </View>
+          <View style={styles.spacesCheckIcon}>
+            {/^\S+$/.test(newPW) ? (
+              <SvgIcon type="greenCheck" />
+            ) : (
+              <SvgIcon type="redX" />
+            )}
+          </View>
+          <View style={styles.spacesCheckText}>
+            <Text style={globalStyles.body2}>No spaces</Text>
+          </View>
+
+          <Pressable
+            style={
+              valid()
+                ? styles.resetContinuePressableAllowed
+                : styles.resetContinuePressable
+            }
+          >
+            <Text
+              style={
+                valid()
+                  ? [globalStyles.overline1, styles.sapphireText]
+                  : [globalStyles.overline1, styles.greyText]
+              }
+              onPress={() =>
+                valid()
+                  ? [
+                      setScreen("success"),
+                      sendEmail("success"),
+                      setUserPassword(confirmPW),
+                    ]
+                  : setPwError(true)
+              }
+            >
+              {valid() ? "CONTINUE" : "RESET PASSWORD"}
+            </Text>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  };
+
+  const successModal = () => {
+    return (
+      <Modal visible={visible} transparent animationType="slide">
+        <View style={styles.modalView}>
+          <View style={styles.topGreyLine} />
+          <Pressable style={styles.exitPressable} onPress={() => reset()}>
+            <Text style={(globalStyles.body1, styles.closeText)}>Close</Text>
+          </Pressable>
+          <Text style={[globalStyles.h2, styles.successTitle]}>
+            You're all set!
+          </Text>
+          <Text style={[globalStyles.h4, styles.successSubTitle]}>
+            Your new password has been set. Check your inbox for confirmation.
+          </Text>
+          <Pressable style={styles.closePressable} onPress={() => reset()}>
+            <Text style={[globalStyles.overline1, styles.whiteText]}>
+              CLOSE
+            </Text>
+          </Pressable>
+        </View>
+      </Modal>
+    );
+  };
+
+  switch (screen) {
+    case "reset":
+      return resetModal();
+    case "verify":
+      return verifyModal();
+    case "setNew":
+      return setNewModal();
+    case "success":
+      return successModal();
+    default:
+      return null;
+  }
+};
