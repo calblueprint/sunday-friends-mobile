c138b045a45077c8a9187dbf674bb42cb34d52ac
Jacob Kim <jacobjk01@berkeley.edu>
2022-04-29 01:39:32 -0700

    dropdown package

--- src/screens/Family/FamilyScreen.tsx
+++ src/screens/Family/FamilyScreen.tsx
@@ -16,6 +16,7 @@ import { AuthenticatedUserContext } from '../../context/userContext';
 import { User } from '../../types/schema';
 import {List} from 'react-native-paper';
 import TransactionsGroup from '../../components/TransactionsGroup/TransactionsGroup';
+import {Collapse,CollapseHeader, CollapseBody, AccordionList} from 'accordion-collapse-react-native';
 
 
 const defaultLabels = ['100', '200', '300']
@@ -55,7 +56,7 @@ const FamilyScreen = ({navigation}: any) => {
     const [labels, setLabels] = useState(['1000', '2000', '3000']);
     const { userUID, setUserUID } = useContext(AuthenticatedUserContext);
     const [user, setUser] = useState(defaultUser);
-
+    const [activeSections, setActiveSections] = useState([]);
     
     //const userID = useContext(userContext);
     
@@ -78,25 +79,8 @@ const FamilyScreen = ({navigation}: any) => {
 
     const tiersStr = []
 
-    return (
-        <View style = {styles.homeContainer}>
-            <ScrollView style = {styles.contentContainer} contentContainerStyle = {styles.alignment}>
-
-            <View style = {styles.dateContainer}>
-                <Text style={[globalStyles.body2, {color: '#525454'}]}>{getCurrentDate()}</Text>
-            </View>
-            <View style = {styles.headline}>
-                <Text style={[globalStyles.h3Bold, {color: '#272929'}]}>Kim Family</Text>
-                <View style={styles.innerContainer}>
-                    <View style={styles.addPersonalCircle}>
-                    <Pressable
-                        onPress={()=>navigation.navigate('Profile')}
-                    >
-                        <Text style={[styles.initialText, {color: '#253C85'}]}>Y</Text>
-                    </Pressable>
-                    </View>
-                </View>
-            </View>
+    const renderHeader = () => {
+        return (
             <View style={styles.familyCardContainer}>
                 <View style={styles.addContainer}>
                     <View style={styles.stackOfSmileys}>
@@ -113,76 +97,119 @@ const FamilyScreen = ({navigation}: any) => {
                     </View>
                 </View>
             </View>
+        )
+    }
 
-            <View style={styles.familyBalanceCardContainer}>
-                <View style={styles.topHalfContainer}>
-                    <View style = {styles.balanceContainer}>
-                        <Text style={[styles.balanceText, {color: "#253C85"}]}>200</Text>
-                        <Text style={globalStyles.overline2}>FAMILY BALANCE</Text>
-                    </View>
-                        <Pressable style={({ pressed }) => [
-                            {
-                                backgroundColor: pressed
-                                ? 'white'
-                                : '#E6ECFE'
-                            },
-                            styles.contributions
-                            ]}>
-                            <Text style={[globalStyles.body2Bold, {color: "#5F5F5F"}]}>View Contributions</Text>
+    const renderContent = () => {
+        return (
+            <View>
+                <Text>Poopy</Text>
+            </View>
+        )
+    }
+
+    const setSections = ( sections: any ) => {
+        setActiveSections(sections);
+    }
+
+    return (
+        <View style = {styles.homeContainer}>
+            <ScrollView style = {styles.contentContainer} contentContainerStyle = {styles.alignment}>
+
+                <View style = {styles.dateContainer}>
+                    <Text style={[globalStyles.body2, {color: '#525454'}]}>{getCurrentDate()}</Text>
+                </View>
+                <View style = {styles.headline}>
+                    <Text style={[globalStyles.h3Bold, {color: '#272929'}]}>Kim Family</Text>
+                    <View style={styles.innerContainer}>
+                        <View style={styles.addPersonalCircle}>
+                        <Pressable
+                            onPress={()=>navigation.navigate('Profile')}
+                        >
+                            <Text style={[styles.initialText, {color: '#253C85'}]}>Y</Text>
                         </Pressable>
-                </View>
-
-                <View style={styles.progressBar}>
-                    <StepIndicator 
-                    stepCount = {3}
-                    customStyles={customStyles}
-                    currentPosition={0}
-                    labels={labels}
-                    />
-
-                </View>
-                <View style={styles.buttomBar}>
-                    <View  style={styles.tierContainer}>
-                        <View style={styles.tierBox}>
-                            <Text style={[globalStyles.overline1, {color: "#FFFFFF"}]}>TIER 1</Text>
-                        </View>
-                        <View style={styles.columnContainer}>
-                            <View style={styles.rowContainer}>
-                                <Text style={[styles.itemsText, {color: "#525454"}]}>[ICON] </Text>
-                                <Text style={[styles.itemsText, {color: "#525454"}]}>Bottle water, can food, fresh food</Text>
-                            </View>
-                            <View style={styles.rowContainer}>
-                                <Text style={[styles.itemsText, {color: "#525454"}]}>[ICON] </Text>
-                                <Text style={[styles.itemsText, {color: "#525454"}]}>Hygeine products, toiletries</Text>
-                            </View>
                         </View>
                     </View>
-                    <View style={styles.tierOptionsContainer}>
-                        <Pressable style={({ pressed }) => [
-                            {
-                                backgroundColor: pressed
-                                ? 'white'
-                                : '#F6F6F6'
-                            },
-                            styles.optionsContainer
-                            ]}>
-                            <Text style={[globalStyles.body2Bold, {color: '#A9A9A9'}]}>Tier Options</Text>
-                        </Pressable>
+                </View>
+
+                <Collapse>
+                    <CollapseHeader>
+                        {renderHeader()}
+                    </CollapseHeader>
+                    <CollapseBody>
+                        {renderContent()}
+                    </CollapseBody>
+                </Collapse>
+
+                <View style={styles.familyBalanceCardContainer}>
+                    <View style={styles.topHalfContainer}>
+                        <View style = {styles.balanceContainer}>
+                            <Text style={[styles.balanceText, {color: "#253C85"}]}>200</Text>
+                            <Text style={globalStyles.overline2}>FAMILY BALANCE</Text>
+                        </View>
+                            <Pressable style={({ pressed }) => [
+                                {
+                                    backgroundColor: pressed
+                                    ? 'white'
+                                    : '#E6ECFE'
+                                },
+                                styles.contributions
+                                ]}>
+                                <Text style={[globalStyles.body2Bold, {color: "#5F5F5F"}]}>View Contributions</Text>
+                            </Pressable>
+                    </View>
+
+                    <View style={styles.progressBar}>
+                        <StepIndicator 
+                        stepCount = {3}
+                        customStyles={customStyles}
+                        currentPosition={0}
+                        labels={labels}
+                        />
+
+                    </View>
+                    <View style={styles.buttomBar}>
+                        <View  style={styles.tierContainer}>
+                            <View style={styles.tierBox}>
+                                <Text style={[globalStyles.overline1, {color: "#FFFFFF"}]}>TIER 1</Text>
+                            </View>
+                            <View style={styles.columnContainer}>
+                                <View style={styles.rowContainer}>
+                                    <Text style={[styles.itemsText, {color: "#525454"}]}>[ICON] </Text>
+                                    <Text style={[styles.itemsText, {color: "#525454"}]}>Bottle water, can food, fresh food</Text>
+                                </View>
+                                <View style={styles.rowContainer}>
+                                    <Text style={[styles.itemsText, {color: "#525454"}]}>[ICON] </Text>
+                                    <Text style={[styles.itemsText, {color: "#525454"}]}>Hygeine products, toiletries</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.tierOptionsContainer}>
+                            <Pressable style={({ pressed }) => [
+                                {
+                                    backgroundColor: pressed
+                                    ? 'white'
+                                    : '#F6F6F6'
+                                },
+                                styles.optionsContainer
+                                ]}>
+                                <Text style={[globalStyles.body2Bold, {color: '#A9A9A9'}]}>Tier Options</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                    <View>
                     </View>
                 </View>
-                <View>
+                <View style={styles.tranxContainer}>
+
+                    <View style = {styles.tranxbar}>
+                        <Text style={[globalStyles.overline1]}>FAMILY TRANSACTIONS</Text>
+                    </View>
+
+                    <TransactionsGroup forFamily = {true}>
+                    </TransactionsGroup>
+
                 </View>
-            </View>
-            <View style={styles.tranxContainer}>
-
-                <View style = {styles.tranxbar}>
-                    <Text style={[globalStyles.overline1]}>FAMILY TRANSACTIONS</Text>
-                </View>
-
-                <TransactionsGroup forFamily = {true}>
-                </TransactionsGroup>
-
-            </View>
             </ScrollView>
         </View>
         
